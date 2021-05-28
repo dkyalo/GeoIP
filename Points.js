7336af91bbed8f6d2d7ad82671af2094ee8ca185
dkyalo <kyalodiana@gmail.com>
2021-05-28 09:06:33 +0300

    Live

---
+++ Points.js
@@ -0,0 +1,27 @@
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {        
+        "ip_address": "173.237.190.55\n", 
+        "country": "United States",
+        "country_code": "US",
+         "continent": "North America",
+         "continent_code": "NA",
+         "city": null, 
+         "county": null,
+          "region": null,
+           "region_code": null, 
+           "postal_code": null, 
+           "timezone": "America/Chicago",
+            "owner": null, 
+            "longitude": -97.822, "latitude": 37.751, 
+            "currency": "USD", 
+            "languages": ["en-US", "es-US", "haw", "fr"]
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-97.822,37.751,]
+    }
+};
+
+
+// "ip_address": "173.237.190.55\n", "country": "United States", "country_code": "US", "continent": "North America", "continent_code": "NA", "city": null, "county": null, "region": null, "region_code": null, "postal_code": null, "timezone": "America/Chicago", "owner": null, "longitude": -97.822, "latitude": 37.751, "currency": "USD", "languages": ["en-US", "es-US", "haw", "fr"]},
